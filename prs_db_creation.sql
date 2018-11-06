d77dc21f2c63cc26a3625bdc3db1a7b71b3877bb
cgrutzmacher <chris.grutzmacher@gmail.com>
2018-11-06 10:33:48 -0500

    database updated

--- prs_db_creation.sql
+++ prs_db_creation.sql
@@ -3,20 +3,40 @@ CREATE DATABASE prs;
 USE prs;
 
 CREATE TABLE User (
-	ID				INT				PRIMARY KEY		AUTO_INCREMENT,
-    userName		VARCHAR(20)		NOT NULL		UNIQUE,
-    password		VARCHAR(10)		NOT NULL,
-    firstName		VARCHAR(20)		NOT NULL,
-    lastName		VARCHAR(20)		NOT NULL,
-    phoneNumber		VARCHAR(12)		NOT NULL,
-    email			VARCHAR(50)		NOT NULL,
-    isReviewer		TINYINT(1)		NOT NULL,
-    isAdmin			TINYINT(1)		NOT NULL,
-    isActive		TINYINT(1)		DEFAULT 1 NOT NULL,
-    dateCreated		DATE			NOT NULL,
-    dateUpdated		DATE			NOT NULL,
-	updatedByUser 	INT				DEFAULT 1 NOT NULL
+	ID					INT				PRIMARY KEY		AUTO_INCREMENT,
+    UserName			VARCHAR(20)		NOT NULL		UNIQUE,
+    Password			VARCHAR(10)		NOT NULL,
+    FirstName			VARCHAR(20)		NOT NULL,
+    LastName			VARCHAR(20)		NOT NULL,
+    PhoneNumber			VARCHAR(12)		NOT NULL,
+    Email				VARCHAR(50)		NOT NULL,
+    IsReviewer			TINYINT(1)		NOT NULL,
+    IsAdmin				TINYINT(1)		NOT NULL,
+    IsActive			TINYINT(1)		DEFAULT 1 NOT NULL,
+    DateCreated			DATETIME		DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    DateUpdated			DATETIME		DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL,
+	UpdatedByUser 		INT				DEFAULT 1 NOT NULL,
+    CONSTRAINT uname UNIQUE (UserName)
 );
+
+CREATE TABLE Vendor (
+	ID					INT				PRIMARY KEY		AUTO_INCREMENT,
+    Code				VARCHAR(10)		NOT NULL,
+    Name				VARCHAR(255)	NOT NULL,
+    Address				VARCHAR(255)	NOT NULL,
+    City				VARCHAR(255)	NOT NULL,
+    `State`				VARCHAR(2)		NOT NULL,
+    Zip					VARCHAR(5)		NOT NULL		UNIQUE,
+    PhoneNumber			VARCHAR(12)		NOT NULL,
+    Email				VARCHAR(100)	NOT NULL,
+    IsPreapproved		TINYINT(1)		NOT NULL,
+    IsActive			TINYINT(1)		DEFAULT 1 NOT NULL,
+    DateCreated			DATETIME		DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    DateUpdated			DATETIME		DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL,
+    UpdatedByUser 		INT				DEFAULT 1 NOT NULL,
+    CONSTRAINT vcode UNIQUE (Code)
+);
+
 
 CREATE TABLE PurchaseRequest (
 	ID					INT				PRIMARY KEY		AUTO_INCREMENT,
@@ -35,3 +55,75 @@ CREATE TABLE PurchaseRequest (
     UpdatedByUser		INT				DEFAULT 1 NOT NULL,
     FOREIGN KEY (UserID) REFERENCES User (ID)
 );
+
+CREATE TABLE Product (
+	ID					INT				PRIMARY KEY		AUTO_INCREMENT,
+    VendorID			INT				NOT NULL,	
+    PartNumber			VARCHAR(50)		NOT NULL,
+    `Name`				VARCHAR(150)	NOT NULL, 
+	Price 				DECIMAL(10,2)	NOT NULL, 
+	Unit 				VARCHAR(255) 	NULL, 
+	PhotoPath 			VARCHAR(255) 	NULL, 
+	IsActive			TINYINT(1)		DEFAULT 1 NOT NULL,
+    DateCreated			DATETIME		DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    DateUpdated			DATETIME		DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL,
+    UpdatedByUser		INT				DEFAULT 1 NOT NULL,
+    
+    
+    FOREIGN KEY (VendorID) REFERENCES Vendor (ID),
+    CONSTRAINT vendor_part UNIQUE (VendorID, PartNumber)    
+);
+
+CREATE TABLE PurchaseRequestLineItem (
+	ID					INT				PRIMARY KEY		AUTO_INCREMENT,
+    PurchaseRequestID	INT				NOT NULL,
+    ProductID			INT				NOT NULL,
+    Quantity			INT				NOT NULL,
+	IsActive			TINYINT(1)		NOT NULL,
+	DateCreated			DATETIME		DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    DateUpdated			DATETIME		DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL,
+    UpdatedByUser 		INT				DEFAULT 1 NOT NULL,
+    
+    FOREIGN KEY	(ProductID) REFERENCES Product (ID),
+    FOREIGN KEY (PurchaseRequestID) REFERENCES PurchaseRequest (ID),
+    CONSTRAINT req_pdt UNIQUE (PurchaseRequestID, ProductID)
+
+);
+
+  -- Add 'SYSTEM' user 
+insert into user (ID, UserName, Password, FirstName, LastName, PhoneNumber, Email, IsReviewer, IsAdmin, UpdatedByUser) VALUES 
+	(1, 'SYSTEM', 'xxxxx', 'System', 'System', 'XXX-XXX-XXXX', 'system@test.com', 0, 0, 1), 
+  	(2, 'cgrutzmacher', 'login', 'chris', 'grutzmacher', '858-444-6457', 'chris.grutzmacher@gmail.com', 1, 1, 1); 
+  
+
+  -- insert some rows into the Vendor table 
+INSERT into vendor (ID, code, name, address, city, state, zip, phonenumber, email, ispreapproved) VALUES 
+  (1, 'BB-1001', 'Best Buy', '100 Best Buy Street', 'Louisville', 'KY', '40207', '502-111-9099', 'geeksquad@bestbuy.com', 0), 
+  (2, 'AP-1001', 'Apple Inc', '1 Infinite Loop', 'Cupertino', 'CA', '95014', '800-123-4567', 'genius@apple.com', 0), 
+  (3, 'AM-1001', 'Amazon', '410 Terry Ave. North', 'Seattle', 'WA', '98109','206-266-1000', 'amazon@amazon.com', 1), 
+  (4, 'ST-1001', 'Staples', '9550 Mason Montgomery Rd', 'Mason', 'OH', '45040', '513-754-0235', 'support@orders.staples.com', 1), 
+  (5, 'MC-1001', 'Micro Center', '11755 Mosteller Rd', 'Sharonville', 'OH', '45241', '513-782-8500', 'support@microcenter.com', 1); 
+  
+
+  -- insert some rows into the Product table 
+INSERT INTO `product` (`ID`,`VendorID`,`PartNumber`,`Name`,`Price`,`Unit`,`PhotoPath`) VALUES (1,1,'ME280LL','iPad Mini 2',296.99,NULL,NULL); 
+INSERT INTO `product` (`ID`,`VendorID`,`PartNumber`,`Name`,`Price`,`Unit`,`PhotoPath`) VALUES (2,2,'ME280LL','iPad Mini 2',299.99,NULL,NULL); 
+INSERT INTO `product` (`ID`,`VendorID`,`PartNumber`,`Name`,`Price`,`Unit`,`PhotoPath`) VALUES (3,3,'105810','Hammermill Paper, Premium Multi-Purpose Paper Poly Wrap',8.99,'1 Ream / 500 Sheets',NULL); 
+INSERT INTO `product` (`ID`,`VendorID`,`PartNumber`,`Name`,`Price`,`Unit`,`PhotoPath`) VALUES (4,4,'122374','HammerMill® Copy Plus Copy Paper, 8 1/2\" x 11\", Case',29.99,'1 Case, 10 Reams, 500 Sheets per ream',NULL); 
+INSERT INTO `product` (`ID`,`VendorID`,`PartNumber`,`Name`,`Price`,`Unit`,`PhotoPath`) VALUES (5,4,'784551','Logitech M325 Wireless Optical Mouse, Ambidextrous, Black ',14.99,NULL,NULL); 
+INSERT INTO `product` (`ID`,`VendorID`,`PartNumber`,`Name`,`Price`,`Unit`,`PhotoPath`) VALUES (6,4,'382955','Staples Mouse Pad, Black',2.99,NULL,NULL); 
+INSERT INTO `product` (`ID`,`VendorID`,`PartNumber`,`Name`,`Price`,`Unit`,`PhotoPath`) VALUES (7,4,'2122178','AOC 24-Inch Class LED Monitor',99.99,NULL,NULL); 
+INSERT INTO `product` (`ID`,`VendorID`,`PartNumber`,`Name`,`Price`,`Unit`,`PhotoPath`) VALUES (8,4,'2460649','Laptop HP Notebook 15-AY163NR',529.99,NULL,NULL); 
+INSERT INTO `product` (`ID`,`VendorID`,`PartNumber`,`Name`,`Price`,`Unit`,`PhotoPath`) VALUES (9,4,'2256788','Laptop Dell i3552-3240BLK 15.6\"',239.99,NULL,NULL); 
+INSERT INTO `product` (`ID`,`VendorID`,`PartNumber`,`Name`,`Price`,`Unit`,`PhotoPath`) VALUES (10,4,'IM12M9520','Laptop Acer Acer™ Aspire One Cloudbook 14\"',224.99,NULL,NULL); 
+INSERT INTO `product` (`ID`,`VendorID`,`PartNumber`,`Name`,`Price`,`Unit`,`PhotoPath`) VALUES (11,4,'940600','Canon imageCLASS Copier (D530)',99.99,NULL,NULL); 
+INSERT INTO `product` (`ID`,`VendorID`,`PartNumber`,`Name`,`Price`,`Unit`,`PhotoPath`) VALUES (12,5,'228148','Acer Aspire ATC-780A-UR12 Desktop Computer',399.99,'','/images/AcerAspireDesktop.jpg'); 
+INSERT INTO `product` (`ID`,`VendorID`,`PartNumber`,`Name`,`Price`,`Unit`,`PhotoPath`) VALUES (13,5,'279364','Lenovo IdeaCentre All-In-One Desktop',349.99,'','/images/LenovoIdeaCenter.jpg'); 
+  
+    -- create a user and grant privileges to that user 
+CREATE USER IF NOT EXISTS 'prs_user'@'localhost' IDENTIFIED BY 'sesame';
+GRANT SELECT, INSERT, DELETE, UPDATE 
+ON prs.* 
+TO 'prs_user'@'localhost';
+  
+  
